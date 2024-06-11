3a6b11c51ce931a5dc1b84f9293925acc8a799d9
geryan <geryan@users.noreply.github.com>
2024-06-11 17:01:24 +1000

    Shot down on the pavement?

---
+++ R/get_intervention.R
@@ -0,0 +1,51 @@
+#' @title Get intervention data
+#' @description
+#' Returns ITN and or IRS data from MAP.
+#'
+#' @param intervention `character` which interventions do you want? all, irs, or
+#'  itn?
+#'
+#' @return `SpatRaster`
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' get_intervention("itn")
+#' }
+get_intervention <- function(
+    intervention = c("itn", "irs"),
+    year = c(2020, 2015)
+  ){
+
+
+
+
+  layers <- data.frame(
+    lyrs = c(
+      "Explorer__2020_Africa_ITN_Use",
+      "Explorer__2015_Nature_Africa_ITN",
+      "Explorer__2020_Africa_IRS_Coverage",
+      "Explorer__2015_Nature_Africa_IRS"
+    ),
+    itv = c(
+      "itn",
+      "itn",
+      "irs",
+      "irs"
+    ),
+    yr = c(
+      2020,
+      2015,
+      2020,
+      2015
+    )
+  ) |>
+    dplyr::filter(
+      itv %in% intervention,
+      yr %in% year
+    ) |>
+    dplyr::pull(lyrs)
+
+  malariaAtlas::getRaster(layers)
+
+}
