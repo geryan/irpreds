0bb6c38560dbb96cefee2fc6a1f3203a25d1265c
geryan <geryan@users.noreply.github.com>
2024-06-11 17:01:35 +1000

    Or waiting on death row?

---
+++ R/all_predictors.R
@@ -0,0 +1,37 @@
+#' @title Collate all predictors
+#' @description
+#' Collates all predictor variables of insecticide resistance, per Hancock et
+#' al. 2017 \url{https://doi.org/10.1371/journal.pbio.3000633}, specifically
+#' table S9 \url{https://doi.org/10.1371/journal.pbio.3000633.s020} (.docx file)
+#'
+#' @param path `character` path to write output.
+#'
+#' @return `SpatRaster`
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' all_predictors()
+#' }
+all_predictors <- function(path = NULL){
+
+  intvn <- get_intervention(intervention = "all")
+
+
+  p <- c(
+    intvn
+  )
+
+  if(!is.null(path)){
+    terra::writeRaster(
+      x = p,
+      filename = sprintf(
+        "%s/predictions.grd",
+        path
+      )
+    )
+  }
+
+  p
+
+}
